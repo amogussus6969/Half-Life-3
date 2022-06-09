2f9b12dbd58fd12e222cb388d652deb99ceced9d
amogussus6969 <samdesl79@gmail.com>
2022-06-09 18:15:01 -0400

    Half-Life 3

---
+++ HALF-LIFE 3.py
@@ -0,0 +1,11 @@
+#HALF-LIFE 3: PYTHON EDITION
+
+#Respect to all Half-Life devs
+#They just need a long rest...
+
+#Importing Modules :)
+import pygame
+import tkinter
+
+
+#Variables
---
+++ playerModule.py
@@ -0,0 +1,12 @@
+#HALF-LIFE 3: PYTHON EDITION
+
+#Importing modules :)
+import pygame
+import tkinter
+import os
+
+#Variables
+
+#Characters
+
+gordon_image_path = os.path.join("images", "")
